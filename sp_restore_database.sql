d448e1b68020828ae5aa13393c6029d14ebd5acf
MohamedAbdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2020-07-25 23:28:22 +0300

    Add files via upload

--- sp_restore_database.sql
+++ sp_restore_database.sql
@@ -1,19 +1,19 @@
 USE [master]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_restore_database]    Script Date: 1/2/2019 11:19:42 AM ******/
+/****** Object:  StoredProcedure [dbo].[sp_restore_database]    Script Date: 7/25/2020 11:20:02 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE Procedure [dbo].[sp_restore_database]
 (
-@backupfile				varchar(max), 
-@filenumber				varchar(5) = 'all', 
-@restore_loc			varchar(500),
+@backupfile			varchar(max), 
+@filenumber			varchar(5) = 'all', 
+@restore_loc			varchar(500)  = 'default',
 @with_recovery			bit = 1,  
 @new_db_name			varchar(500) = 'default',
-@percent				int = 5,
-@password				varchar(100) = 'default')
+@percent			int = 5,
+@password			varchar(100) = 'default')
 as
 begin 
 declare @restor_loc_table table (output_text varchar(max))
@@ -31,6 +31,7 @@ declare
 @Position				int, 
 @DatabaseName			varchar(500), 
 @BackupType				int,
+@BackupTypeDescription  varchar(100),
 @lastfile				int
 
 declare @headeronly table (
@@ -99,7 +100,7 @@ col01 varchar(max),col02 varchar(max),col03 varchar(max),col04 varchar(max),
 col05 varchar(max),col06 varchar(max),col07 varchar(max),col08 varchar(max),
 col09 varchar(max),col10 varchar(max),col11 varchar(max),col12 varchar(max),
 col13 varchar(max),col14 varchar(max),col15 varchar(max),col16 varchar(max),
-col17 varchar(max),col18 varchar(max))
+col17 varchar(max),col18 varchar(max),col19 varchar(max))
 
 set nocount on
 if @password = 'default'
@@ -129,6 +130,7 @@ when @@version like '%SQL Server 2008%' then 10
 when @@version like '%SQL Server 2012%' then 11 
 when @@version like '%SQL Server 2014%' then 12 
 when @@version like '%SQL Server 2016%' then 13 
+when @@version like '%SQL Server 2017%' then 14 
 end
 
 if @version = 10
@@ -183,10 +185,9 @@ or output_text like '%.ldf%'))a)b
 inner join (select reverse(substring(reverse(PhysicalName),1,charindex('\',reverse(physicalname))-1)) filelist from @filelistonly) fl
 on b.restore_loc_files = fl.filelist
 
-
 declare backupfiles_cursor cursor fast_forward for
-select Position, DatabaseName, BackupType
-from @headeronly
+select Position, DatabaseName, BackupType, BackupTypeDescription
+from @headeronly 
 where Position between 
 case when @filenumber = 'all' then 0 else @filenumber end
 and
@@ -210,20 +211,23 @@ fetch next from dbfiles_cursor into @logicalname, @originalpath, @physicalname, 
 while @@fetch_status = 0
 begin
 
-if @restore_loc = @originalpath
+if @restore_loc = 'default'
 begin
 set @file_move = isnull(@file_move+',','')+'
 MOVE N'+''''+@logicalname+''''+' TO N'+''''+@originalpath+@physicalname+'__'+@unique_id+@ext+''''
 end
-else if @files_exist > 0
-begin
-set @file_move = isnull(@file_move+',','')+'
-MOVE N'+''''+@logicalname+''''+' TO N'+''''+@restore_loc+@physicalname+'__'+@unique_id+@ext+''''
-end
 else
 begin
-set @file_move = isnull(@file_move+',','')+'
-MOVE N'+''''+@logicalname+''''+' TO N'+''''+@restore_loc+@physicalname+@ext+''''
+	if @files_exist > 0
+	begin
+		set @file_move = isnull(@file_move+',','')+'
+		MOVE N'+''''+@logicalname+''''+' TO N'+''''+@restore_loc+@physicalname+'__'+@unique_id+@ext+''''
+	end
+	else
+	begin
+		set @file_move = isnull(@file_move+',','')+'
+		MOVE N'+''''+@logicalname+''''+' TO N'+''''+@restore_loc+@physicalname+@ext+''''
+	end
 end
 
 fetch next from dbfiles_cursor into @logicalname, @originalpath, @physicalname, @ext
@@ -232,7 +236,7 @@ close dbfiles_cursor
 deallocate dbfiles_cursor 
 
 open backupfiles_cursor 
-fetch next from backupfiles_cursor into @Position, @DatabaseName, @BackupType
+fetch next from backupfiles_cursor into @Position, @DatabaseName, @BackupType, @BackupTypeDescription
 while @@fetch_status = 0
 begin
 
@@ -240,12 +244,16 @@ if @password = 'default'
 begin
 set @sql = '
 RESTORE '+
-case @BackupType when 1 then 'DATABASE' when 2 then 'LOG' end+' '+
+case @BackupType 
+when 1 then @BackupTypeDescription 
+when 2 then substring(@BackupTypeDescription, charindex(' ',@BackupTypeDescription)+1, len(@BackupTypeDescription))
+when 5 then substring(@BackupTypeDescription, 1, charindex(' ',@BackupTypeDescription)-1)
+end+' '+
 case when @new_db_name = 'default' then '['+@DatabaseName+']' else '['+@new_db_name+']' end
 +'
 FROM DISK = N'+''''+@backupfile+''''+'
 WITH FILE = '+cast(@Position as varchar)+','+
-case when @BackupType ! = 2 then @file_move+',' else '' end+'
+case when @BackupType = 1 then @file_move+',' else '' end+'
 '+case 
 when @filenumber  = 'all' and @lastfile = @position then 
 case when @with_recovery = 1 then 'RECOVERY' else 'NORECOVERY' end
@@ -257,12 +265,16 @@ else
 begin
 set @sql = '
 RESTORE '+
-case @BackupType when 1 then 'DATABASE' when 2 then 'LOG' end+' '+
+case @BackupType 
+when 1 then @BackupTypeDescription 
+when 2 then substring(@BackupTypeDescription, charindex(' ',@BackupTypeDescription)+1, len(@BackupTypeDescription))
+when 5 then substring(@BackupTypeDescription, 1, charindex(' ',@BackupTypeDescription)-1)
+end+' '+
 case when @new_db_name = 'default' then '['+@DatabaseName+']' else '['+@new_db_name+']' end
 +'
 FROM DISK = N'+''''+@backupfile+''''+'
 WITH FILE = '+cast(@Position as varchar)+','+
-case when @BackupType ! = 2 then @file_move+',' else '' end+'
+case when @BackupType = 1 then @file_move+',' else '' end+'
 '+case 
 when @filenumber  = 'all' and @lastfile = @position then 
 case when @with_recovery = 1 then 'RECOVERY' else 'NORECOVERY' end
@@ -271,10 +283,11 @@ case when @with_recovery = 1 then 'RECOVERY' else 'NORECOVERY' end
 else 'NORECOVERY' end+', NOUNLOAD, MEDIAPASSWORD= '+''''+@password+''''+', STATS = '+cast(@percent as varchar)
 end
 
---print(@sql)
+set @sql = replace(@sql, 'MSSQL14.DBAMI1','MSSQL14.DBAMI3')
+print(@sql)
 exec(@sql)
 
-fetch next from backupfiles_cursor into @Position, @DatabaseName, @BackupType
+fetch next from backupfiles_cursor into @Position, @DatabaseName, @BackupType, @BackupTypeDescription
 end
 close backupfiles_cursor 
 deallocate backupfiles_cursor 
