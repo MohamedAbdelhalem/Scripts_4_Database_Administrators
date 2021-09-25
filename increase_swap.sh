b08857f961f877940513377e7888c9ef91217f0d
MohamedAbdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-09-25 23:03:20 +0300

    Update increase_swap.sh

--- increase_swap.sh
+++ increase_swap.sh
@@ -1,5 +1,5 @@
-dir=$1
-siz=$2
+dir=$1          #parameter1 means the directory /swap
+siz=$2          #parameter2 means the swap file size = 1GB
 swaparr=()
 nofiles=()
 siz=$((siz * 1024))
@@ -17,10 +17,10 @@ fi
 
 curr=$(($(echo "${nofiles[*]}" | sort -nr | head -n1) + 1))
 
-dd if=/dev/zero of=/swap/swapfile$curr bs=1M count=$siz
-mkswap /swap/swapfile$curr
-chmod 600 /swap/swapfile$curr
-echo "/swap/swapfile$curr         swap                    swap    defaults        0 0" >> /etc/fstab
+dd if=/dev/zero of=/$dir/swapfile$curr bs=1M count=$siz
+mkswap /$dir/swapfile$curr
+chmod 600 /$dir/swapfile$curr
+echo "/$dir/swapfile$curr         swap                    swap    defaults        0 0" >> /etc/fstab
 swapon -a
 swapon -s
 free -h
