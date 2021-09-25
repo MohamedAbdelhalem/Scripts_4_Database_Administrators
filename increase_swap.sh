08979ac32d2078b60ff17de4d789aa75025c949d
MohamedAbdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-09-25 23:32:50 +0300

    Update increase_swap.sh

--- increase_swap.sh
+++ increase_swap.sh
@@ -1,4 +1,4 @@
-#[root@localhost ~]# sh exist.sh Swap 1
+#[root@localhost ~]# sh increase_swap.sh Swap 1
 #1024+0 records in
 #1024+0 records out
 #1073741824 bytes (1.1 GB) copied, 1.19677 s, 897 MB/s
