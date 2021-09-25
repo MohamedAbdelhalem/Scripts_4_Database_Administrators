8089d68ceb0d07350f7de41e4936785a1e8ad58b
MohamedAbdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-09-25 23:32:10 +0300

    Update increase_swap.sh

--- increase_swap.sh
+++ increase_swap.sh
@@ -1,3 +1,14 @@
+#[root@localhost ~]# sh exist.sh Swap 1
+#1024+0 records in
+#1024+0 records out
+#1073741824 bytes (1.1 GB) copied, 1.19677 s, 897 MB/s
+#Setting up swapspace version 1, size = 1048572 KiB
+#no label, UUID=598033bc-a829-4dd8-8bb9-de95e196d7df
+#Mem:           974M        109M         63M        6.6M        800M        706M
+#Swap:            9G          0B          9G
+###################################################################################
+#copy the below script into your sh file
+###################################################################################
 dir=$1          #parameter1 means the directory /swap
 siz=$2          #parameter2 means the swap file size = 1GB
 swaparr=()
