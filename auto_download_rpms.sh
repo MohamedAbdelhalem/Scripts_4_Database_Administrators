85c9704e3b72e8a8faccd65000ef149c54ea63a8
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-11-29 00:59:44 +0300

    Update auto_download_rpms.sh

--- auto_download_rpms.sh
+++ auto_download_rpms.sh
@@ -1,31 +1,27 @@
-#you already have a curl from a website like curl postgresql.org/dowmload > ~/postgreSQL.repo.txt
-#and you need to download all the rpms automatically 
+#and you need to download all or some rpms automatically 
 #so you can use the below script 😉
 #examples
 #
-#sh ~/downloadPostgreSQLFiles.sh download sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
-#sh ~/downloadPostgreSQLFiles.sh download all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
-#sh ~/downloadPostgreSQLFiles.sh view sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
-#sh ~/downloadPostgreSQLFiles.sh view all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/auto_download_rpms.sh download sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/auto_download_rpms.sh download all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/auto_download_rpms.sh view sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/auto_download_rpms.sh view all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
 
 action=$1               #view or download
 search=$2               #all or a name in the packages like sqlite or python3
 url=$3                  #the original URL https://download.postgresql.org/pub/repos/yum/12/redhat 
-                        #that you already curl of into the txt file ~/postgreSQL.repo.txt 
-                        #https://github.com/MohamedAbdelhalem/Scripts_4_Database_Administrators/blob/master/postgreSQL.repo.txt
-                        #👆🏻
-                        
-postrpms=($(cat ~/postgreSQL.repo.txt | grep .rpm))
+
+postrpms=($(curl $url | grep .rpm))
 filesno=$(echo ${#postrpms[@]})
 files=()
 no=0
-echo $filesno
+#echo $filesno
 for ((c=0; c< $filesno; c++)); do
         file=$(echo ${postrpms[$c]} | cut -d ">" -f1)
         file=${file:6:1000}
         if [[ ${file:0:1} != "-" && $file == *".rpm"* ]]; then
                 file=$(echo ${file:0:$((${#file}-1))})
-                echo $file
+                #echo $file
                 files[$no]+=$file
                 no=$((no + 1))
         fi
