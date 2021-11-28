48b3ec7337ffa87912f2870477bdf78b7abf74b6
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-11-28 13:19:19 +0300

    Update auto_download_rpms.sh

--- auto_download_rpms.sh
+++ auto_download_rpms.sh
@@ -1,8 +1,18 @@
 #you already have a curl from a website like curl postgresql.org/dowmload > ~/postgreSQL.repo.txt
 #and you need to download all the rpms automatically 
 #so you can use the below script :)
+#examples
+#
+#sh ~/downloadPostgreSQLFiles.sh download sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/downloadPostgreSQLFiles.sh download all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/downloadPostgreSQLFiles.sh view sqlite https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
+#sh ~/downloadPostgreSQLFiles.sh view all https://download.postgresql.org/pub/repos/yum/12/redhat/rhel-8-x86_64/
 
-url=$1
+action=$1               #view or download
+search=$2               #all or a name in the packages like sqlite or python3
+url=$3                  #the original URL https://download.postgresql.org/pub/repos/yum/12/redhat 
+                        #that you already curl of into the txt file ~/postgreSQL.repo.txt 
+
 postrpms=($(cat ~/postgreSQL.repo.txt | grep .rpm))
 filesno=$(echo ${#postrpms[@]})
 files=()
@@ -19,5 +29,17 @@ for ((c=0; c< $filesno; c++)); do
         fi
 done
 for ((u=0; u<$(echo ${#files[@]}); u++)); do
-        curl -O $url${files[$u]}
+        if [[ $search == "all" ]]; then
+                if [[ $action == "view" ]]; then
+                        echo curl -O $url${files[$u]}
+                elif [[ $action == "download" ]]; then
+                        curl -O $url${files[$u]}
+                fi
+        else
+                if [[ $action == "view" && $(echo ${files[$u]}) == *"$search"* ]]; then
+                        echo curl -O $url${files[$u]}
+                elif [[ $action == "download" && $(echo ${files[$u]}) == *"$search"* ]]; then
+                        curl -O $url${files[$u]}
+                fi
+        fi
 done
