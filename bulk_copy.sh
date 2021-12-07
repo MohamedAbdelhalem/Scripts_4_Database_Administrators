697420708dc3bd95c31d12ddbd490e616b232910
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-12-07 13:07:50 +0300

    Update bulk_copy.sh

--- bulk_copy.sh
+++ bulk_copy.sh
@@ -1,6 +1,9 @@
-#we need to copy 150 python files from the current folder to new one
+#we need to copy 150 python files from the current folder "./" or another folder to new one
+#
 #sh bulk_copy.sh .py view ./ /oracle/home/python_scripts
 #sh bulk_copy.sh .py copy ./ /oracle/home/python_scripts
+#sh bulk_copy.sh .py view /postgres/home/newproject/scripts/ /oracle/home/python_scripts
+#sh bulk_copy.sh .py copy /postgres/home/newproject/scripts/ /oracle/home/python_scripts
 
 search=$1
 action=$2
