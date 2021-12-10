e7a58b9be7ef6c1a01d666d446f536d20fcf4907
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-12-10 16:07:35 +0300

    Update sizefolders.sh

--- sizefolders.sh
+++ sizefolders.sh
@@ -1,17 +1,56 @@
-nofolders=($(ls -lh | grep "^d" | wc -l))
-readarray folders < <(ls -l | grep "^d")
-for((c=0; c<$nofolders; c++)); do
-    foldername=$(echo "${folders[$c]}")
-    f=${foldername:53:100}
-    siz="./"$f" size "
-    len=${#siz}
-    len=$((54 - $len))
-    char=$(printf "#%.0s" $(seq $len))
-    if [[ "$f" = *" "* ]]; then
-        d=$(du -ch ./Tibco' 'SRC | grep total)
-            echo "./"$f" size "$char" "$d
-    else
-            d=$(du -ch ./$f | grep total)
-            echo "./"$f" size "$char" "$d
-    fi
+folder=$1
+v=0
+size=0
+no=($(ls $folder | grep "^d" | wc -l))
+folders=($(ls -l $folder | grep "^d"))
+detail=""
+print=""
+toprint=0
+sfolde=""
+size=""
+hh="-"
+rr=""
+sf=0
+for ((c = 0; c < ${#folders[@]}; c++)); do
+        if [[ ${folders[$c]} == *"drwx"* ]]; then
+                if [ $v == 0 ]; then
+                        toprint=0
+                else
+                        toprint=1
+                fi
+                print=$detail
+                v=1
+                detail=${folders[$c]}
+        else
+                v=$(($v + 1))
+                if [[ $v > 10 ]]; then
+                        detail=$detail" "${folders[$c]}
+                else
+                        detail=$detail","${folders[$c]}
+                fi
+                toprint=0
+        fi
+        if [ $toprint == 1 ]; then
+                sfolder=$(echo $print | cut -d " " -f9)
+                if [[ $sfolder == *","* ]]; then
+                        sfolder=$(echo $sfolder | tr "," " ")
+                        size=$(du -Dh | grep "./$sfolder" | grep "\<$sfolder\>")
+                else
+                        size=$(du -ch $sfolder | grep total)
+                fi
+                size=$(echo $size | cut -d " " -f1)
+                sf=$(( 20 - ${#size}))
+                printf "%s %"$sf"d %s\n" $size "" "$sfolder"
+        fi
 done
+sfolder=$(echo $detail | cut -d " " -f9)
+if [[ $sfolder == *","* ]]; then
+        sfolder=$(echo $sfolder | tr "," " ")
+        size=$(du -Dh | grep "./$sfolder" | grep "\<$sfolder\>")
+else
+        size=$(du -ch $sfolder | grep total)
+fi
+size=$(echo $size | cut -d " " -f1)
+sf=$(( 20 - ${#size}))
+printf "%s %"$sf"d %s\n" $size "" "$sfolder"
+
