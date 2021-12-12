1b6bdfcd0f0b5878447e3e366a061ab29bc3faf0
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-12-12 15:26:21 +0300

    Update whatismyip.sh

--- Shell/whatismyip.sh
+++ Shell/whatismyip.sh
@@ -1,5 +1,6 @@
 mask=$1
-net=$(ls /etc/sysconfig/network-scripts/)
+net=$(ls /etc/sysconfig/network-scripts/ | grep ifcfg)
+net=$(echo $net | cut -d " " -f1)
 net=$(echo $net | cut -d "-" -f2)
 net=$(ip a | grep $net | grep /$mask)
 net=$(echo $net | cut -d " " -f2)
