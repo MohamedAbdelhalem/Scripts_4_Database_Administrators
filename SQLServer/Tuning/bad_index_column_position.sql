e84db131c8d1adb6e7c0e8c8b36fc30b7f9a37b9
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2021-12-15 10:30:59 +0300

    Update bad_index_column_position.sql

--- SQLServer/Tuning/bad_index_column_position.sql
+++ SQLServer/Tuning/bad_index_column_position.sql
@@ -6,12 +6,18 @@
 
 --just alter the result of the indexes and reorder the keys and remove the fill factor = 100%
 
-select identity_column_position, index_column_name, is_identity, index_id, index_name, object_id, table_name, index_type, 
+declare @result table (
+[database_name] varchar(400), identity_column_position int, index_column_name varchar(300), is_identity bit, index_id int, index_name varchar(400), object_id int, 
+table_name varchar(400), index_type varchar(100), WRONG_FillFactor_Val int, RIGHT_FillFactor_Val int, is_unique bit, is_unique_constraint bit)
+
+insert into @result
+exec sp_MSforeachdb 'USE [?]
+select db_name(db_id()) database_name, identity_column_position, index_column_name, is_identity, index_id, index_name, object_id, table_name, index_type, 
 WRONG_FillFactor_Val, RIGHT_FillFactor_Val, is_unique, is_unique_constraint 
 from (
 select row_number() over(partition by i.name, i.object_id order by i.object_id, i.name) identity_column_position,
-c.name index_column_name,c.is_identity, i.index_id, isnull(i.name,'') index_name, t.object_id, 
-'['+schema_name(t.schema_id)+'].['+t.name+']' table_name, 
+c.name index_column_name,c.is_identity, i.index_id, isnull(i.name,'''') index_name, t.object_id, 
+''[''+schema_name(t.schema_id)+''].[''+t.name+'']'' table_name, 
 i.type_desc index_type, fill_factor WRONG_FillFactor_Val , 0 RIGHT_FillFactor_Val, is_unique, is_unique_constraint  
 from sys.indexes i inner join sys.tables t
 on i.object_id = t.object_id
@@ -22,10 +28,12 @@ inner join sys.columns c
 on c.object_id = ic.object_id
 and c.column_id = ic.column_id
 where fill_factor between 1 and 99
-and ic.is_included_column = 0
-and (is_unique = 1 or is_unique_constraint = 1))a
+and ic.is_included_column = 0)a
 where is_identity = 1 
 and identity_column_position != 1
-order by table_name, index_name
+order by table_name, index_name'
 
+select * 
+from @result
+where db_id([database_name]) > 4
 
