d33ff4c924bb47ba28a0c694486a2297a3f229bc
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2022-02-24 13:06:10 +0300

    Update Indexed_view_eligibility.sql

--- SQLServer/Indexed_view_eligibility.sql
+++ SQLServer/Indexed_view_eligibility.sql
@@ -1,6 +1,9 @@
-select 
-table_name, 
-case when sum(isnull(COLUMNPROPERTY(object_id(table_name), column_name, 'IsDeterministic'),0)) > 0 then 'does not eligable to create indexed view' else 'okay' end [status]
-from INFORMATION_SCHEMA.COLUMNS
-group by table_name
 
+select 
+o.name, sum(COLUMNPROPERTY(o.object_id, c.column_name, 'IsDeterministic')),
+isnull(case when sum(COLUMNPROPERTY(o.object_id, c.column_name, 'IsDeterministic')) > 0 then 'does not eligable to create indexed view' else 'okay' end,'not a view') [status]
+from INFORMATION_SCHEMA.COLUMNS c inner join sys.objects o
+on o.name = c.table_name
+where o.name = 'view name'
+group by o.object_id, o.name
+order by status
