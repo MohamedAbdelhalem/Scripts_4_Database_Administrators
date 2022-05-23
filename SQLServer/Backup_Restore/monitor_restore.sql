829d377d2bf4a91b8dc29d4c63c741e1b2d838ae
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2022-05-23 11:29:22 +0300

    Update monitor_restore.sql

--- SQLServer/Backup_Restore/monitor_restore.sql
+++ SQLServer/Backup_Restore/monitor_restore.sql
@@ -1,17 +1,25 @@
-select spid, round(percent_complete,4) percent_complete,
+select spid, percent_complete,
 case
 when s.text like '%restore database%' and s.text like '%move%' then 'FULL'
 when s.text like '%restore database%' and s.text not like '%move%' then 'DIFF'
 when s.text like '%restore log%' then 'LOG' end [restore_type], 
-dbo.duration(datediff(s, r.start_time, getdate())) duration, 
-dbo.duration(
-cast((100.0 / round(percent_complete,4)) 
+dbo.duration('s',datediff(s, r.start_time, getdate())) duration, 
+dbo.duration('s',
+case when percent_complete = 0 then 0 else case when 
+cast((100.0 / (round(percent_complete,5) + .00001)) 
 * 
 datediff(s, r.start_time, getdate()) as int)
 -
 datediff(s, r.start_time, getdate())
+< 0 then 0 else
+cast((100.0 / (round(percent_complete,5) + .00001)) 
+* 
+datediff(s, r.start_time, getdate()) as int)
+-
+datediff(s, r.start_time, getdate())
+end end
 ) time_to_complete,
-dbo.duration(estimated_completion_time/1000) estimated_completion_time,
+dbo.duration('s', estimated_completion_time/1000) estimated_completion_time,
 s.text, waittime, lastwaittype, blocked, command, r.status
 from sys.sysprocesses p cross apply sys.dm_exec_sql_text(p.sql_handle)s
 left outer join sys.dm_exec_requests r
@@ -19,5 +27,6 @@ on p.spid = r.session_id
 inner join sys.dm_exec_connections c
 on p.spid = c.session_id
 where command like 'Restore%'
+
 
 
