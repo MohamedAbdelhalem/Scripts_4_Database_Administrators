56f64b9815970abdb7aa4a289d96d8355701ebc1
Mohamed Fawzy Abdelhalem <46447150+MohamedAbdelhalem@users.noreply.github.com>
2022-06-15 16:02:17 +0300

    Update sp_index_detail.sql

--- SQLServer/Indexes/sp_index_detail.sql
+++ SQLServer/Indexes/sp_index_detail.sql
@@ -95,28 +95,3 @@ when 2 then 'NONCLUSTERED'
 end, @sql
 end
 
-go
-
-declare @object_id bigint, @index_id bigint
-declare @indexes table (id int identity(1,1), table_name varchar(500), index_id int, index_name varchar(1000), index_type varchar(100), synatx varchar(max))
-declare x cursor fast_forward
-for
---select t.object_id, '['+schema_name(t.schema_id)+'].['+t.name+']' table_name,t.type_desc, i.name index_name, i.index_id, i.type_desc, is_primary_key
-select t.object_id, i.index_id
-from sys.tables t left outer join sys.indexes i
-on t.object_id = i.object_id
-where is_primary_key = 0
-and i.type_desc != 'HEAP'
-order by object_id
-
-open x
-fetch next from x into @object_id, @index_id
-while @@FETCH_STATUS = 0
-begin
-
-insert into @indexes (table_name, index_id, index_name, index_type, synatx)
-exec [dbo].[sp_index_detail] @P_object_id = @object_id,  @p_index_id = @index_id
-fetch next from x into @object_id, @index_id
-end
-close x
-deallocate x
